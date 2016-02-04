95b6ca257d114b759b29b3fa6bc7018bdcd01a50
Timo Dörr <timo@latecrew.de>
2016-02-04 16:04:08 +0000

    Avoid ambient module declaration
    The .d.ts file is now a non-ambient module definition

--- src/pubsub-interfaces.ts
+++ src/pubsub-interfaces.ts
@@ -1,118 +1,115 @@
-namespace PubSubA {
+export interface IPubSub {
+  start(callback?: IPubSubStartCallback, disconnect?: Function);
+  stop(callback?: IPubSubStopCallback);
 
-  export interface IPubSub {
-    start (callback?: IPubSubStartCallback, disconnect?: Function);
-    stop (callback?: IPubSubStopCallback);
+  channel(name: string, callback?: IChannelReadyCallback): IChannel;
 
-    channel (name: string, callback?: IChannelReadyCallback): IChannel;
-
-    /*
-    It is not possible to declare an interface member method as static in TypeScript - so bare in
-    mind that any implementation must have this .includeIn() method here as static method
-    includeIn: IPubSubOperationsMixin;
-    */
-  }
-
-  /**
-  @description includeIn function that has to be present on the implementation that implements
-    IPubSub.
+  /*
+  It is not possible to declare an interface member method as static in TypeScript - so bare in
+  mind that any implementation must have this .includeIn() method here as static method
+  includeIn: IPubSubOperationsMixin;
   */
-  export interface IPubSubOperationsMixin {
-    (obj: any, publish_name?: string, subscribe_name?: string): any;
-  }
+}
+
+/**
+@description includeIn function that has to be present on the implementation that implements
+  IPubSub.
+*/
+export interface IPubSubOperationsMixin {
+  (obj: any, publish_name?: string, subscribe_name?: string): any;
+}
+
+/**
+@description Callback that fires when the main PubSub class is ready
+*/
+export interface IPubSubStartCallback {
+  (status: any, error: any): void;
+}
+
+export interface IPubSubStopCallback {
+  (status: any, error: any);
+}
+
+export interface IChannelReadyCallback {
+  (channel: IChannel): void;
+}
+
+export interface IPubSubOperations {
+
+  publish<T>(topic: string, payload: T, callback?: IPublishReceivedCallback<T>): void;
 
   /**
-  @description Callback that fires when the main PubSub class is ready
-  */ 
-  export interface IPubSubStartCallback {
-    (status: any, error: any): void;
-  }
-
-  export interface IPubSubStopCallback {
-    (status: any, error: any);
-  }
-
-  export interface IChannelReadyCallback {
-    (channel: IChannel): void;
-  }
-
-  export interface IPubSubOperations {
-
-    publish<T> (topic: string, payload: T, callback?: IPublishReceivedCallback<T>): void;
-
-    /**
-    @description Subscribe to a topic.
-    @param callback - If given, the callback will be executed after the server has
-      confirmed that the subscription was sucessfully put in place
-    */
-    subscribe<T> (topic: string, subscription: ISubscriptionFunc<T>,
-      callback?: ISubscriptionRegisteredCallback<T>) : ISubscriptionToken;
-
-    // subscribe and dispose subscription after removal
-    once<T> (topic: string, subscription: ISubscriptionFunc<T>,
-      callback?: ISubscriptionRegisteredCallback<T>) : ISubscriptionToken;
-
-  }
-
-  /**
-  @description A communication channel used for topic grouping.
+  @description Subscribe to a topic.
+  @param callback - If given, the callback will be executed after the server has
+    confirmed that the subscription was sucessfully put in place
   */
-  export interface IChannel extends IPubSubOperations {
-    name: string;
-  }
+  subscribe<T>(topic: string, subscription: ISubscriptionFunc<T>,
+    callback?: ISubscriptionRegisteredCallback<T>): ISubscriptionToken;
 
-  /**
-  @description Class that helps cleaning up subscriptions
-  */
-  export interface ISubscriptionToken {
-
-    /* will remove the subscription */
-    dispose: (callback?: SubscriptionDisposedCallback) => number;
-
-    /**
-    @description Indicates whether this subscription was already dispose by calling .dispose().
-      Any subsequent calls to dispose() are an error and will result in an exception.
-    */
-    isDisposed: boolean;
-
-    /**
-    @description Number of LOCAL subscriptions at the time of subscribing - minimum will be 1
-      as the own subscription is counted in
-    */
-    count: number;
-  }
-
-  export interface disposeFunction {
-    (SubscriptionDisposedCallback): number;
-  }
-
-  export interface SubscriptionDisposedCallback {
-    /* callback once the subscription is disposed. Passes the number of remaining subscriptions.
-      If a backend doesnt support that, the first argument should be undefined.
-     */
-    (number?): void;
-  }
-
-  /**
-  @description Argument that is passed to any .subscribe() function and
-  executed upon publishes
-  */
-  export interface ISubscriptionFunc<T> {
-    (payload: T): void;
-  }
-
-
-  /**
-  @description Callback that fires after a subscription was received by the
-    main subscription instance. In networking environments, the callback will
-    fire after the server has successfully registered the subscription.
-  */
-  export interface ISubscriptionRegisteredCallback<T> {
-    (dispose: ISubscriptionToken, topic: string, subscription: ISubscriptionFunc<T>);
-  }
-
-  export interface IPublishReceivedCallback<T> {
-    (): any;
-  }
+  // subscribe and dispose subscription after removal
+  once<T>(topic: string, subscription: ISubscriptionFunc<T>,
+    callback?: ISubscriptionRegisteredCallback<T>): ISubscriptionToken;
 
 }
+
+/**
+@description A communication channel used for topic grouping.
+*/
+export interface IChannel extends IPubSubOperations {
+  name: string;
+}
+
+/**
+@description Class that helps cleaning up subscriptions
+*/
+export interface ISubscriptionToken {
+
+  /* will remove the subscription */
+  dispose: (callback?: SubscriptionDisposedCallback) => number;
+
+  /**
+  @description Indicates whether this subscription was already dispose by calling .dispose().
+    Any subsequent calls to dispose() are an error and will result in an exception.
+  */
+  isDisposed: boolean;
+
+  /**
+  @description Number of LOCAL subscriptions at the time of subscribing - minimum will be 1
+    as the own subscription is counted in
+  */
+  count: number;
+}
+
+export interface disposeFunction {
+  (SubscriptionDisposedCallback): number;
+}
+
+export interface SubscriptionDisposedCallback {
+  /* callback once the subscription is disposed. Passes the number of remaining subscriptions.
+    If a backend doesnt support that, the first argument should be undefined.
+   */
+  (number?): void;
+}
+
+/**
+@description Argument that is passed to any .subscribe() function and
+executed upon publishes
+*/
+export interface ISubscriptionFunc<T> {
+  (payload: T): void;
+}
+
+
+/**
+@description Callback that fires after a subscription was received by the
+  main subscription instance. In networking environments, the callback will
+  fire after the server has successfully registered the subscription.
+*/
+export interface ISubscriptionRegisteredCallback<T> {
+  (dispose: ISubscriptionToken, topic: string, subscription: ISubscriptionFunc<T>);
+}
+
+export interface IPublishReceivedCallback<T> {
+  (): any;
+}
+
