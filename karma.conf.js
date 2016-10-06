8e3528a1c49cbe2465f899dc5d655635c72c7742
Timo Dörr <timo@latecrew.de>
2016-10-06 16:52:17 +0200

    Increate timeout for browser tests

--- karma.conf.js
+++ karma.conf.js
@@ -12,6 +12,12 @@ module.exports = function(config) {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha', 'chai'],
 
+    client: {
+      mocha: {
+        timeout: 5000
+      }
+    },
+
 
     // list of files / patterns to load in the browser
     files: [
