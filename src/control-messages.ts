8bdbdb0406d69cd282886864e5024d5fdc37bdbe
Timo Dörr <timo@latecrew.de>
2020-01-13 13:54:35 +0100

    Add control command type

--- src/control-messages.ts
+++ src/control-messages.ts
@@ -25,8 +25,27 @@ export interface UnsubscribeUsageStats {
     type: "UNSUBSCRIBE_USAGE_STATS";
 }
 
+/**
+ * Informational message that are sent to the client
+ */
 export type ControlMessage = ClientDisconnect | UsageStats;
+
+/**
+ * Commands a client might send to the server. Reserved for implementation specific
+ * commands
+ */
+export type ControlCommand = never;
+
+/**
+ * Subscribe to specific mesages which will be received via the ControlMessage
+ * channel
+ */
 export type ControlSubscribe = SubscribeDisconnect | SubscribeUsageStats;
+
+/**
+ * Unsubcribe to specific mesages which will be received via the ControlMessage
+ * channel
+ */
 export type ControlUnsubscribe = UnsubscribeDisconnect | UnsubscribeUsageStats;
 
 export interface ControlChannelSubscribeMap {
@@ -36,4 +55,5 @@ export interface ControlChannelSubscribeMap {
 export interface ControlChannelPublishMap {
     CONTROL_SUBSCRIBE: ControlSubscribe;
     CONTROL_UNSUBSCRIBE: ControlUnsubscribe;
+    CONTROL_COMMAND: ControlCommand;
 }
